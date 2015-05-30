68fdb77fde352c59673bbb41f36807184dc7570b
mind <minddnim@gmail.com>
2015-05-30 13:54:44 +0900

    capture image of webcam displayed.

--- StudyOpenCV/CaptureFromCAM/captureFromCAM.hs
+++ StudyOpenCV/CaptureFromCAM/captureFromCAM.hs
@@ -1,9 +1,19 @@
-import CV.Image
+import Control.Monad
 import CV.Video
 import CV.HighGUI
 
+windowName :: String
+windowName = "camera Window"
+
+capture :: Capture -> IO ()
+capture srcCam = do
+  Just frame <- getFrame srcCam
+  showImage windowName frame
+  keyVal <- waitKey 33
+  unless (keyVal == 27) $ capture srcCam
+
 main :: IO()
 main = do
+  makeWindow windowName
   Just srcCam <- captureFromCam 1
-  Just frame <- getFrame srcCam
-  display frame
+  capture srcCam
